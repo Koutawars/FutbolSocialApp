9104cd356120582c87bd72d100dbf41d468881b6
Koutawars <admikouta@gmail.com>
2020-09-22 18:32:47 -0500

    Agregando bootstrap e instalando react-router-dom

--- views/src/App.js
+++ views/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col md="auto">1 of 1</Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
