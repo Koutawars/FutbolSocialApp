ae9938fdd0852926b8e40814aa02589262485e6d
Koutawars <admikouta@gmail.com>
2020-09-23 18:11:05 -0500

    Instalando nuevas librerias y arrreglando el css

--- views/src/pages/Login.js
+++ views/src/pages/Login.js
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom';
 import React, {Component} from 'react';
+import logo from '../Images/Logo.png'
 
-import { Container, Form, Button } from 'react-bootstrap';  
+import { Container, Form, Button, Row, Col, Image} from 'react-bootstrap';  
 class Login extends Component {
     constructor(props){
         super(props);
@@ -19,31 +20,37 @@ class Login extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-
         console.log("Correo: " + this.state.email + " Contraseña: " + this.state.password);
     }
     render(){
         return (
             <Container md="auto">
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group controlId="email">
-                        <Form.Label>Correo</Form.Label>
-                        <Form.Control onChange = {this.handleChange} type="email" placeholder="Escribir correo" />
-                        <Form.Text className="text-muted">
-                        Nosotros no compartimos tu correo con nadie mas.
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group controlId="password">
-                        <Form.Label>Contraseña</Form.Label>
-                        <Form.Control onChange = {this.handleChange} type="password" placeholder="Contraseña" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Enviar
-                    </Button>
-                    <Form.Text className="text-muted">
-                        ¿No tiene una cuenta? Registrese <Link to="/registrar">aquí.</Link>
-                    </Form.Text>
-                </Form>
+                <Row className="justify-content-md-center">
+                    <Col xs lg="5">
+                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                            <Image width="200px" src={logo} rounded />
+                        </div>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group controlId="email">
+                                <Form.Label>Correo</Form.Label>
+                                <Form.Control onChange = {this.handleChange} type="email" placeholder="Escribir correo" />
+                                <Form.Text className="text-muted">
+                                Nosotros no compartimos tu correo con nadie mas.
+                                </Form.Text>
+                            </Form.Group>
+                            <Form.Group controlId="password">
+                                <Form.Label>Contraseña</Form.Label>
+                                <Form.Control onChange = {this.handleChange} type="password" placeholder="Contraseña" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Enviar
+                            </Button>
+                            <Form.Text className="text-muted">
+                                ¿No tiene una cuenta? Registrese <Link to="/registrar">aquí.</Link>
+                            </Form.Text>
+                        </Form>
+                    </Col>
+                </Row>
             </Container>
         );
     }
