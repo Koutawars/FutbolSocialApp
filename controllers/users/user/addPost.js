a2b818b94c20338599fc3970ed102f9eb1fef483
anderson420 <44132519+anderson420@users.noreply.github.com>
2020-11-10 17:47:08 -0500

    Update addPost.js

--- controllers/users/user/addPost.js
+++ controllers/users/user/addPost.js
@@ -3,13 +3,19 @@ const Field = require('../../../models/Post');
 const addPost = async (req, res) => {
     let {imagen, pie_post, eventos_id} = req.body;
     var usuario_id = req.tokenInfo.id;
-    let field = await Field.create({
-        usuario_id,
-        imagen,
-        pie_post,
-        eventos_id
-    })
-    res.json({field});
+    if(piepie_post){
+        let field = await Field.create({
+            usuario_id,
+            imagen,
+            pie_post,
+            eventos_id
+        })
+        res.json({field});
+
+    }else{
+        res.status(400);
+    }
+    
 }
 
 module.exports = addPost;
