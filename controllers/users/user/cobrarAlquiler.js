f1a21e57ca3c6c393a586fbb387b4b2fc8930488
Koutawars <admikouta@gmail.com>
2020-12-09 14:32:03 -0500

    comentado

--- controllers/users/user/cobrarAlquiler.js
+++ controllers/users/user/cobrarAlquiler.js
@@ -4,11 +4,13 @@ const cobrarAlquiler = async (req, res) => {
     let id =  req.tokenInfo.id;
     let {resta_saldo} =req.params;
     let field;
+    /*
     field = await Field.update({
         saldo=saldo-resta_saldo
     }, {where: { [Op.and]: [{ id }, { saldo:{[Op.eq]: resta_saldo} }]}
 
     });
+    */
     
     res.json({field});
 }
